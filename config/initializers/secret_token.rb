c225726785bb609f31913e3ee6fda5c969bc9de2
Nicolas Rolland <nicolas.rolland@gmail.com>
2011-06-04 20:44:03 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socksbook::Application.config.secret_token = '92a73d7941564103e78cd3093add9b5d5c60b27a57c7d96c20acb9bed43c802d2bf50045caa12ac40c14f4a9dc3081fe96ab7329c458584c0867f73d380dca2d'
