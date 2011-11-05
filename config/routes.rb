968c4245dc09d2fc3634cc8dfa1713f9896f9360
Nicolas Rolland <nicolas.rolland@gmail.com>
2011-11-05 12:00:44 +0100

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Socksbook::Application.routes.draw do
+  resources :socks
+
+  get "say/hello"
+
+  get "say/goodbye"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
